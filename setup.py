220e3167292c6ba0899beafb7f500dd40457248a
stevenbennett96 <stevenbennettonline@gmail.com>
2020-05-01 12:58:44 +0100

    Updated.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stko",
+    version="0.0.1",
+    author="Steven Bennett",
+    author_email="s.bennett18@imperial.ac.uk",
+    description="A module containing calculators for use within stk.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JelfsMaterialsGroup/stk_optimizers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
